e77cef4d8e56b0fdade898a90d15b54625f7316b
Thomas Nowotny <t.nowotny2sussex.ac.uk>
2021-04-01 11:51:11 +0100

    version used for producing report2.
    s Please enter the commit message for your changes. Lines starting

--- src/plot_raster.py
+++ src/plot_raster.py
@@ -7,7 +7,7 @@ def plot_raster(spike_t, spike_ID,t_total,N,bname,display= True):
     plt.figure()
     if spike_t is not None:
         plt.plot(spike_t, spike_ID, '.', markersize= 1)
-    plt.title(pop)
+    plt.title(bname)
     plt.xlim([0, t_total])
     plt.ylim([0, N])
     plt.savefig(bname+"_spikes.png",dpi=300)
@@ -17,14 +17,13 @@ def plot_raster(spike_t, spike_ID,t_total,N,bname,display= True):
 if __name__ == "__main__":
 
     argv= sys.argv
-    if len(argv) != 5:
+    if len(argv) != 4:
         print("usage: python plot_raster.py <base name> <t_total> <N>")
         exit()
 
-    label= argv[1]
-    pop= argv[2]
-    t_total= float(argv[3])
-    N= int(argv[4])
+    bname= argv[1]
+    t_total= float(argv[2])
+    N= int(argv[3])
     spike_t= np.load(bname+"_spike_t.npy")
     spike_ID= np.load(bname+"_spike_ID.npy")
     plot_raster(spike_t, spike_ID, t_total, N, bname)
