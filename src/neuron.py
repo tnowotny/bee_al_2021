017dc74f262ccda8768019febdaa0cc82d7921e4
Thomas Nowotny <t.nowotny2sussex.ac.uk>
2021-04-12 21:52:41 +0100

    Some homogenisation of parameters; including trying to have inhibition beyond the phasic response (removing adaptation from LNs).

--- src/neuron.py
+++ src/neuron.py
@@ -53,16 +53,16 @@ pn_ini = {"V": -60.0,
           }
 
 
-ln_params = {"C_mem": 1.0,
+ln_params = {"C_mem": 2.0,
              "V_reset": -70.0,
-             "V_thresh": -35.0,
+             "V_thresh": -40.0,
              "V_leak": -60.0,
              "g_leak": 0.01,
              "r_scale": 1.0,
-             "g_adapt": 0.005,
+             "g_adapt": 0.0,
              "V_adapt": -70.0,
              "tau_adapt": 1000.0,
-             "noise_A": 0.7/np.sqrt(sim.dt)
+             "noise_A": 1.4/np.sqrt(sim.dt)
              }
 
 ln_ini = {"V": -60.0,
