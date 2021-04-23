f50b7a42d352b1e459b113aff352df873314057a
Thomas Nowotny <t.nowotny2sussex.ac.uk>
2021-04-23 22:00:00 +0100

    Added a fifth experiment where two specific odours are tested in a mixture; this is to check whether the observations match the experiments that the monotonic odours persists in the mixture, even if the non-monotonic odour is reasonably highly concentrated.

---
+++ src/exp5.py
@@ -0,0 +1,153 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from helper import *
+from exp1_plots import exp1_plots
+from ALsim import ALsim
+import sim
+import sys
+import os
+from ALsimParameters import std_paras
+import random
+
+"""
+experiment to investigate the effect of decreasing response with higher concentration. In this version we generate N_odour odours randomly with the following properties: 
+1. Each odour has a a Gaussian profile of glomerulus activation with sigma drawn from Gauss(mu_sig,sig_sig). 
+2. the Gaussian odour profile is over a random permutation of the glomeruli.
+"""
+
+N_odour= 100
+mu_sig= 8
+sig_sig= 2
+
+if len(sys.argv) < 2:
+    print("usage: python exp5.py <run#>")
+    exit()
+
+ino= int(sys.argv[1])
+paras= std_paras()
+
+if ino == -1:
+    paras["lns_pns_g"]= 0
+    paras["lns_lns_g"]= 0
+else:
+    paras["lns_pns_g"]*= 0.1*np.power(np.sqrt(10),ino)
+    paras["lns_lns_g"]*= 0.1*np.power(np.sqrt(10),ino)
+
+# write results into a dir with current date in the name
+timestr = time.strftime("%Y-%m-%d")
+paras["dirname"]= timestr+"-runs"
+
+path = os.path.isdir(paras["dirname"])
+if not path:
+    print("making dir "+paras["dirname"])
+    os.makedirs(paras["dirname"])
+
+paras["plotting"]= False
+paras["plotdisplay"]= False
+paras["use_spk_rec"]= True
+
+paras["rec_state"]= [
+#    ("ORs", "ra"),
+#    ("ORNs", "V"),
+#    ("ORNs", "a"),
+#    ("PNs", "V")
+]
+
+paras["rec_spikes"]= [
+#    "ORNs",
+    "PNs",
+#    "LNs"
+    ]
+
+paras["plot_raster"]= [
+#    "ORNs",
+    "PNs",
+#    "LNs"
+    ]
+
+paras["plot_sdf"]= {
+#    "ORNs": range(74,86,2),
+    "PNs": list(range(74,87,2)),
+#    "LNs": list(range(74,87,2))
+    }
+
+label= "two_odours"
+paras["label"]= label+"_"+str(ino)
+
+# Assume a uniform distribution of Hill coefficients inspired by Rospars'
+# work on receptors tiling the space of possible sigmoid responses
+
+hill_new= False
+
+if hill_new:
+    hill_exp= np.random.uniform(0.5, 1.5, paras["n_glo"])
+    np.save(paras["dirname"]+"/"+label+"_hill",hill_exp)
+else:
+    hill_exp= np.load(paras["dirname"]+"/"+label+"_hill.npy")
+
+# Let's do a progression of broadening odours
+odor_new= False
+
+if odor_new:
+    odors= None
+    odor_sigma= np.array([ 1.0, 10.0 ])
+    for i in range(N_odour):
+        sigma= random.gauss(mu_sig,sig_sig)
+        od= gauss_odor(paras["n_glo"], 0, sigma)
+        random.shuffle(od)
+        print(od)
+        if odors is None:
+            odors= np.copy(od)
+        else:
+            odors= np.vstack((odors, np.copy(od)))
+    np.save(paras["dirname"]+"/"+label+"_odors",odors)
+else:
+    odors= np.load(paras["dirname"]+"/"+label+"_odors.npy")
+    oNo= odors.shape[0]
+
+correl= np.corrcoef(odors,rowvar=False)
+correl= correl/2.0+1.0
+for i in range(paras["n_glo"]):
+    correl[i,i]= 0.0
+
+# Now, let's make a protocol where each odor is presented for 3 secs with
+# 3 second breaks and at each of 24 concentration values
+paras["protocol"]= []
+t_off= 3000.0
+base= np.power(10,0.25)
+
+o1= 13
+o2= 30
+for c1 in range(24):
+    for c2 in range(24):
+        sub_prot= {
+            "t": t_off,
+            "odor": o1,
+            "ochn": "0",
+            "concentration": 1e-7*np.power(base,c1),
+        }
+        paras["protocol"].append(sub_prot)
+        sub_prot["odor"]= o2
+        sub_prot["ochn"]= "1"
+        paras["protocol"].append(sub_prot)        
+        sub_prot= {
+            "t": t_off+3000.0,
+            "odor": o1,
+            "ochn": "0",
+            "concentration": 0.0,
+        }
+        paras["protocol"].append(sub_prot)
+        sub_prot["odor"]= o2
+        sub_prot["ochn"]= "1"
+        paras["protocol"].append(sub_prot)
+        t_off+= 6000.0;
+
+paras["t_total"]= t_off
+print("We are running for a total simulated time of {}ms".format(t_off))
+
+if __name__ == "__main__":
+    state_bufs, spike_t, spike_ID= ALsim(odors, hill_exp, paras, lns_gsyn= correl)
+
+    if paras["plotting"]:
+        exp1_plots(state_bufs, spike_t, spike_ID, paras, display=plotdisplay)
