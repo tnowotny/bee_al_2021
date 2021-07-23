1251fd95b1b3c63f63a92aef898d1d00d7c14c50
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2021-07-23 22:14:12 +0100

    Some minor error fixes.

--- src/scan_geo_breadth.py
+++ src/scan_geo_breadth.py
@@ -18,7 +18,6 @@ The overall strength of inhibition is scaled by a command line argument "ino".
 
 paras= std_paras()
 paras["write_to_disk"]= True
-paras["geoshift"]= 38
 if len(sys.argv) < 3:
     print("usage: python experiment1.py <ino> <connect_I: hom/corr0/corr1/cov0/cov1")
     exit()
@@ -76,7 +75,6 @@ for A in range(3):
     for a in range(-1,2):
         for b in range(-5,6):
             od= gauss_odor(paras["n_glo"], paras["n_glo"]//2+paras["geo_shift"], np.power(bbase,b)*paras["geo_sigma"], A+paras["geo_A"], paras["odor_clip"], np.power(abase,a)*paras["geo_act"], 0.0, 1e-10, 1.0)
-            od[:,0]= od[the_shuffle,0]
             odors.append(np.copy(od))
 paras["N_odour"]= len(odors)
 odors= np.array(odors)
